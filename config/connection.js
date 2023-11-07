a3cf2399aed237aec2e389b100ed168b98a779d2
Danny Collan <collan97@gmail.com>
2023-11-06 22:28:50 -0800

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,8 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
